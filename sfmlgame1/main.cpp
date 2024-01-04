cb5ad76e04cfb79268d1e38984702d8fa14d1d60
casvalrem123 <thsaud814@gmail.com>
2024-01-04 15:21:46 +0900

    Initial Commit

---
+++ sfmlgame1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sfmlgame1
+//
+//  Created by Sunmyoung Yun on 2024/01/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
